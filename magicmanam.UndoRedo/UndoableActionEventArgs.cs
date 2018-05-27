d88ff6061aeec9e17c10350ff73efa7c25a9831b
Andrei Muski <magicmanam@gmail.com>
2018-05-27 21:33:38 +0300

    Add project files.

---
+++ magicmanam.UndoRedo/UndoableActionEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace magicmanam.UndoRedo
+{
+    public class UndoableActionEventArgs
+    {
+        public UndoableActionEventArgs(string action)
+        {
+            Action = action;
+        }
+
+        public string Action { get; private set; }
+    }
+}
---
+++ magicmanam.Windows/Messages.cs
@@ -0,0 +1,12 @@
+﻿namespace magicmanam.Windows
+{
+	public static class Messages
+    {
+		public static int WM_CREATE = 0x0001;
+		public static int WM_HOTKEY = 0x0312;
+		public static int WM_DRAWCLIPBOARD = 0x308;
+		public static int WM_DESTROY = 0x0002;
+		public static int WM_CHANGECBCHAIN = 0x030D;
+        public static int WM_WINDOWPOSCHANGED = 0x47;
+    }
+}
