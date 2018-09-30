d9263e0f8d64f7afca2b9d929281d660a575a38b
Andrei Muski <magicmanam@gmail.com>
2018-09-30 23:11:15 +0300

    1) Adds Windows messages definition; 2) Updates RemoteShell class; 3) Commits RemoteManagement nuspec file.

--- magicmanam.RemoteManagement/RemoteShell.cs
+++ magicmanam.RemoteManagement/RemoteShell.cs
@@ -7,28 +7,75 @@ namespace magicmanam.RemoteManagement
 {
     public class RemoteShell : IRemoteShell
     {
-        private readonly PowerShell _ps;
+        private readonly string _server;
 
-        private RemoteShell(PowerShell ps, string computerName)
+        private PowerShell _powerShell;
+
+        private IServicesShell _services;
+        private IEventViewerShell _events;
+
+        /// <summary>
+        /// Initializes a new instance of the RemoteShell class.
+        /// </summary>
+        /// <param name="computerName">The name of the computer on which to connect.</param>
+        public RemoteShell(string server)
         {
-            this._ps = ps;
-
-            Services = new ServicesShell(ps, computerName);
-            Events = new EventViewerShell(computerName);
+            this._server = server;
         }
 
-        public IServicesShell Services { get; private set; }
-
-        public IEventViewerShell Events { get; private set; }
-
-        public static IRemoteShell Create(string computerName)
+        /// <summary>
+        /// Gets the PowerShell instance used in the current RemoteShell instance.
+        /// </summary>
+        protected PowerShell PS
         {
-            return new RemoteShell(PowerShell.Create(), computerName);
+            get
+            {
+                if (this._powerShell == null)
+                {
+                    this._powerShell = PowerShell.Create();
+                }
+
+                return this._powerShell;
+            }
+        }
+
+        /// <summary>
+        /// Gets the IServiceShell instance to work with Windows services. Works based on PowerShell .NET wrapper.
+        /// </summary>
+        public IServicesShell Services
+        {
+            get
+            {
+                if (this._services == null)
+                {
+                    this._services = new ServicesShell(this.PS, this._server);
+                }
+
+                return this._services;
+            }
+        }
+
+        /// <summary>
+        /// Gets the IEventViewerShell instance to work with Windows Event Viewer events.
+        /// </summary>
+        public IEventViewerShell Events {
+            get
+            {
+                if (this._events == null)
+                {
+                    this._events = new EventViewerShell(this._server);
+                }
+
+                return this._events;
+            }
         }
 
         public void Dispose()
         {
-            this._ps.Dispose();
+            if (this._powerShell != null)
+            {
+                this._powerShell.Dispose();
+            }
         }
     }
 }
